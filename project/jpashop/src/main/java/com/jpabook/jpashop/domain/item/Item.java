27dd9280048a95665cc0dc3d9e538de3917790d2
055055 <snb4301@gmail.com>
2022-07-13 17:40:06 +0900

    [스프링부트와JPA활용1] 엔티티 클래스 개발1

---
+++ project/jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,22 @@
+package com.jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Entity
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
--- project/jpashop/src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
+++ project/jpashop/src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop;
 
+import com.jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ class MemberRepositoryTest {
     public void testMember() throws Exception {
         //givn
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
@@ -28,7 +29,7 @@ class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
