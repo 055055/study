4ff9b493a9bf71c2d8292a0dba6fa66f89e05b5a
055055 <snb4301@gmail.com>
2022-09-06 00:18:29 +0900

    [재고시스템으로 알아보는 동시성이슈 해결방법] Synchronized 이용해보기

--- project/stock/src/main/java/com/example/stock/service/StockService.java
+++ project/stock/src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -12,7 +13,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+//    @Transactional시 실제 트랜잭션 종료시키기 전에 다른 메서드가 커밋하기 전 데이터 (syncronized메서드에 접근해서 얻은 값)에 접근해서 데이터를 업데이트 시켜서 데이터가 꼬일 수 있다.
+    public synchronized void decrease(Long id, Long quantity) {
         //get stock
         Stock stock = stockRepository.findById(id).orElseThrow();
         //재고 감소
